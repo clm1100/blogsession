f0d263abbdf52a718d52dded65ec1e1455b67e39
clm110 <275298711@qq.com>
2018-05-11 23:42:52 +0800

    图灵机器人

--- super.js
+++ super.js
@@ -1,1 +1,14 @@
-var superagent = require('superagent')
+var S = require('superagent')
+var apikey="c396dbc1dadd4fd09c61b4a2ae65ba9a";
+var url = "http://www.tuling123.com/openapi/api";
+
+var obj = {
+    key:"c396dbc1dadd4fd09c61b4a2ae65ba9a",
+    info:"你是谁",
+    userid: "123456"
+}
+S.post(url).send(obj).end(function(err,data,body){
+    console.log(err)
+    console.log(data)
+    console.log(body)
+})
