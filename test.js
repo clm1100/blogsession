33fa4d95bfd49ae93c831af9fbf76560066bff50
clm110 <275298711@qq.com>
2018-05-16 19:12:11 +0800

    了解os模块

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
-var qs1 = require('querystring');
-var qs = require('qs');
-var obj = {
-    a:1,
-    b:2,
-    c:{
-        r:22
-    },
-    d:[1,2,3,5,6]
-}
+// var qs1 = require('querystring');
+// var qs = require('qs');
+// var obj = {
+//     a:1,
+//     b:2,
+//     c:{
+//         r:22
+//     },
+//     d:[1,2,3,5,6]
+// }
 
-console.log(qs1.stringify(obj));
-console.log(qs.stringify(obj))
-console.log(qs.parse(qs.stringify(obj)))
+// console.log(qs1.stringify(obj));
+// console.log(qs.stringify(obj))
+// console.log(qs.parse(qs.stringify(obj)))
+var os = require('os');
+console.log(os.tmpdir())
+console.log(os.cpus())
+console.log(os.platform())
